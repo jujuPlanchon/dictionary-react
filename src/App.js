fef9728f98da7c2918788203b641b2816ee5e7ad
jujuPlanchon <juliafoplanchon@gmail.com>
2022-03-03 16:26:07 +0100

    added new component and form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import Wild from "./Wild.png";
+import Dictionary from "./Dictionary";
+import "./Dictionary.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={Wild} className="img-fluid App-logo" alt="logo" />
-        <h1>Dictionary App</h1>
-      </header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <img src={Wild} className="img-fluid App-logo" alt="logo" />
+          <h1 className="mt-5">Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Julia Planchon</footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Type the word..."
+          onChange={handleKeywordChange}
+        />
+      </form>
+    </div>
+  );
+}
