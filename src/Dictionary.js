c1ab05eedef4f02964763cb5d8231645bcd8dcc1
jujuPlanchon <juliafoplanchon@gmail.com>
2022-03-04 13:29:52 +0100

    added axios and Api imigration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {}
+
   function search(event) {
     event.preventDefault();
-    alert`${keyword}`;
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
